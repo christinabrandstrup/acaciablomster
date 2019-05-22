a9ee717043d005c72b5f487be1efd403048dcd1c
christinabrandstrup <44491893+christinabrandstrup@users.noreply.github.com>
2019-05-22 10:59:45 +0200

    Upload af child theme

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/*Denne fil er en del af divi-child, Divi child theme.
+
+Alle funktionerne i denne fil vil blive indlæst før det overordnet temas funktioner.
+Lær mere af https://codex.wordpress.org/Child_Themes.
+
+Note: denne funktion indlæser det overordnet temas stylesheet før undertemaets stylesheet
+(lad være tomt med mindre du ved hvad du laver.)
+*/
+
+function divi_child_enqueue_child_styles() {
+$parent_style = 'parent-style'; 
+	wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 
+		'child-style', 
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get('Version') );
+	}
+add_action( 'wp_enqueue_scripts', 'divi_child_enqueue_child_styles' );
+
+/*Skriv her dine egne funktioner */
